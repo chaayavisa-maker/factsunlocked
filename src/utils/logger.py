ff013d3862eab6f84e26d393faf8c1001c0632f3
chaayavisa-maker <chaaya.visa@gmail.com>
2026-05-17 12:40:25 +0200

    Rename setup_logger to get_logger and update formatting

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,14 +1,14 @@
 import logging
 import sys
 
-def setup_logger(name: str) -> logging.Logger:
+
+def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     if not logger.handlers:
         handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(logging.Formatter(
-            "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
-            datefmt="%H:%M:%S"
-        ))
+        handler.setFormatter(
+            logging.Formatter("%(asctime)s [%(levelname)s] %(name)s: %(message)s")
+        )
         logger.addHandler(handler)
         logger.setLevel(logging.INFO)
     return logger
